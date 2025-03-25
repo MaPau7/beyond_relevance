c5e187fc4cb57bc4079723e09c3c408311a76651
Maria Paula Cortes-Lemos <116693427+MaPau7@users.noreply.github.com>
2025-03-25 05:55:00 -0700

    Updated scripts to include different DPR models

---
+++ full_run_updated/Retriever/similarity/contriever.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from transformers import AutoTokenizer, AutoModel
+import torch
+from Retriever.base import BaseRetriever
+from Retriever.utils import create_embeddings
+
+class ContrieverRetriever(BaseRetriever):
+    def load_model(self):
+        """Loads the Contriever model."""
+        tokenizer = AutoTokenizer.from_pretrained("facebook/contriever")
+        model = AutoModel.from_pretrained("facebook/contriever")
+        return tokenizer, model
+
+    def embed_texts(self, texts, text_type):
+        """Contriever uses mean pooling over all tokens."""
+        return create_embeddings(self.tokenizer, self.model, texts, method="mean_pooling")
+
+    # def embed_texts(self, texts):
+    #     """Generate embeddings using mean pooling."""
+    #     return create_embeddings(self.tokenizer, self.model, texts)
+
